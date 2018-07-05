b35aee7567bb87de972ac296ba4044df84f2b0b3
Andrii <aabrasha@gmail.com>
2018-07-05 13:47:19 +0300

    Create try-cats.sh

---
+++ try-cats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+COURSIER_URL=https://git.io/vgvpD
+test -e ~/.coursier/coursier || \
+  (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
+~/.coursier/coursier launch -q -P \
+  com.lihaoyi:ammonite_2.12.4:1.0.3 \
+  org.typelevel:cats-core:1.1.0 \
+  -- --predef-code 'import cats._, cats.data._, cats.implicits._' < /dev/tty
