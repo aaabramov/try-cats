8c1fc00ffc4b336cf8216647cacb5c17a7e71f5e
Andrii <aabrasha@gmail.com>
2018-07-05 13:50:44 +0300

    Add _2.12

--- try-cats.sh
+++ try-cats.sh
@@ -4,5 +4,5 @@ test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P \
   com.lihaoyi:ammonite_2.12.4:1.0.3 \
-  org.typelevel:cats-core:1.1.0 \
+  org.typelevel:cats-core_2.12:1.1.0 \
   -- --predef-code 'import cats._, cats.data._, cats.implicits._' < /dev/tty
